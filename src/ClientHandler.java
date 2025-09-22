8d30317a143569fe75a628ff4fdd3811b1c8fce6
Rachel Davey <racheld9999@outlook.com>
2025-09-22 23:51:06 +0200

    client handler

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -16,12 +18,22 @@ public class ClientHandler implements Runnable {
     public ClientHandler(Socket socket) {
         try {
             this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputSteam()));
+            this.bufferedReader = new BufferedReader(new OutputStreamWriter(socket.getInputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            //add client to arraylist so they can join gc
+            clientHandlers.add(this);
+            broadcastMessage("SERVER: " + clientUsername + " has entered the chat!");
+
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
 
 
     @Override
     public void run() {
+
 
     }
 }
