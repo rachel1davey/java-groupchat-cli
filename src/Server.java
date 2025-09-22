908755455db168659184cdd8a1e3591c999c40f9
Rachel Davey <racheld9999@outlook.com>
2025-09-22 23:28:52 +0200

    inital commit

---
+++ src/Server.java
@@ -0,0 +1,21 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+    public void startServer() {
+        try {
+
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected!");
+                ClientHandler clientHandler = new ClientHandler();
+            }
+
+        }
+    }
+}
