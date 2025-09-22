9ef4bd88d9ddd2cc59ae3c7442258dadd997d307
Rachel Davey <racheld9999@outlook.com>
2025-09-23 00:22:57 +0200

    Fix typo in ClientHandler.java: getOutputSteam -> getOutputStream

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -15,7 +15,7 @@ public class ClientHandler implements Runnable {
     public ClientHandler(Socket socket) {
         try {
             this.socket = socket;
-            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputSteam()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.clientUsername = bufferedReader.readLine();
             //add client to arraylist so they can join gc
